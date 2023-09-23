7dd1428305e5c6249e5d213b18b67877bb21754c
Carla <berdincarla@gmail.com>
2023-09-23 20:14:02 +0800

    ui: create fragments. setup navigation component

---
+++ app/src/main/java/com/codev/recruitment/carlaberdin/fragment/AddEditContactFragment.java
@@ -0,0 +1,29 @@
+package com.codev.recruitment.carlaberdin.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codev.recruitment.carlaberdin.R;
+
+public class AddEditContactFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_edit_contact, container, false);
+    }
+}
---
+++ app/src/main/java/com/codev/recruitment/carlaberdin/fragment/ContactSummaryFragment.java
@@ -0,0 +1,32 @@
+package com.codev.recruitment.carlaberdin.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codev.recruitment.carlaberdin.R;
+
+public class ContactSummaryFragment extends Fragment {
+
+    public ContactSummaryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact_summary, container, false);
+    }
+}
