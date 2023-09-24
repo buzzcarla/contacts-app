1a0a87539a02b638fb4b3e65bceb822810255f5f
Carla <berdincarla@gmail.com>
2023-09-24 12:57:13 +0800

    feat: ui: display selected contact details in ContactSummary with databinding

--- app/src/main/java/com/codev/recruitment/carlaberdin/fragment/ContactSummaryFragment.java
+++ app/src/main/java/com/codev/recruitment/carlaberdin/fragment/ContactSummaryFragment.java
@@ -2,19 +2,26 @@ package com.codev.recruitment.carlaberdin.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codev.recruitment.carlaberdin.R;
+import com.codev.recruitment.carlaberdin.databinding.FragmentContactSummaryBinding;
+import com.codev.recruitment.carlaberdin.repository.data.Contact;
+import com.codev.recruitment.carlaberdin.vm.ContactViewModel;
 
 public class ContactSummaryFragment extends Fragment {
 
-    public ContactSummaryFragment() {
-        // Required empty public constructor
-    }
+    private FragmentContactSummaryBinding binding;
+
+    private ContactViewModel mContactVM;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,21 @@ public class ContactSummaryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_summary, container, false);
+
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_contact_summary, container, false);
+
+        View view = binding.getRoot();
+        //here data must be an instance of the class MarsDataProvider
+
+        mContactVM = new ViewModelProvider(requireActivity()).get(ContactViewModel.class);
+
+        binding.setContact(mContactVM.getCurrentlyViewing());
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
