3820582aade926e6c178d419b072e912e04b79ed
Carla <berdincarla@gmail.com>
2023-09-24 14:32:25 +0800

    feat: ui: change favorite status with databinding

--- app/src/main/java/com/codev/recruitment/carlaberdin/vm/ContactViewModel.java
+++ app/src/main/java/com/codev/recruitment/carlaberdin/vm/ContactViewModel.java
@@ -3,8 +3,10 @@ package com.codev.recruitment.carlaberdin.vm;
 import android.app.Application;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.ObservableField;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.codev.recruitment.carlaberdin.lib.ContactsLib;
 import com.codev.recruitment.carlaberdin.repository.data.Contact;
@@ -16,7 +18,7 @@ public class ContactViewModel extends AndroidViewModel {
 
     private ContactsLib mContactsLib;
 
-    private Contact mCurrentlyViewing;
+    private MutableLiveData<Contact> mCurrentlyViewing;
 
 
     public ContactViewModel(@NonNull Application application) {
@@ -24,17 +26,31 @@ public class ContactViewModel extends AndroidViewModel {
 
         mContactsLib = new ContactsLib(application);
 
+        mCurrentlyViewing = new MutableLiveData<>();
+
+    }
+
+    public void addContact(Contact contact) {
+        mContactsLib.insertContact(contact);
+    }
+    public void deleteContact(Contact contact) {
+        mContactsLib.deleteContact(contact);
+    }
+
+    public void updateContact(Contact contact) {
+        mContactsLib.updateContact(contact);
     }
 
     public LiveData<List<Contact>> getAllContacts() {
         return mContactsLib.getAllContacts();
     }
 
-    public Contact getCurrentlyViewing() {
+    public MutableLiveData<Contact> getCurrentlyViewing() {
         return mCurrentlyViewing;
     }
 
     public void setCurrentlyViewing(Contact currentlyViewing) {
-        this.mCurrentlyViewing = currentlyViewing;
+        this.mCurrentlyViewing.postValue(currentlyViewing);
     }
+
 }
