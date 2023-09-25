afd4a162ac356aa063247e5859c1317c0ba60df4
Carla <berdincarla@gmail.com>
2023-09-25 16:16:07 +0800

    feat: working add/edit contact

--- app/src/main/java/com/codev/recruitment/carlaberdin/fragment/AddEditContactFragment.java
+++ app/src/main/java/com/codev/recruitment/carlaberdin/fragment/AddEditContactFragment.java
@@ -1,19 +1,34 @@
 package com.codev.recruitment.carlaberdin.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codev.recruitment.carlaberdin.R;
 import com.codev.recruitment.carlaberdin.databinding.FragmentAddEditContactBinding;
+import com.codev.recruitment.carlaberdin.repository.data.Contact;
+import com.codev.recruitment.carlaberdin.utils.Util;
 import com.codev.recruitment.carlaberdin.vm.ContactViewModel;
 
 public class AddEditContactFragment extends Fragment {
@@ -21,6 +36,11 @@ public class AddEditContactFragment extends Fragment {
     private FragmentAddEditContactBinding binding;
 
     private ContactViewModel mContactVM;
+
+    private NavController navController;
+
+    private final int ALERT_SAVE = 0;
+    private final int ALERT_DISCARD = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,6 +70,194 @@ public class AddEditContactFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        navController = Navigation.findNavController(view);
 
+        // Handle phone back press
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /*enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                // Handle the back button event
+                onBack();
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
+
+        if (mContactVM.getCurrentlyViewing().getValue() != null && mContactVM.getCurrentlyViewing().getValue().getImage() != null) {
+            // Edit contact, show image if not null
+            mContactVM.setCapturedImage(Util.decodeBase64ToBitmap(mContactVM.getCurrentlyViewing().getValue().getImage()));
+        }
+        binding.imgContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open camera
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraActivityResultLauncher.launch(cameraIntent);
+            }
+        });
+
+        binding.btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.popBackStack();
+            }
+        });
+
+        binding.btnSave.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String firstName = binding.edittextFirstName.getText().toString();
+                String lastName = binding.edittextLastName.getText().toString();
+                String phone = binding.edittextPhone.getText().toString();
+                String email = binding.edittextEmail.getText().toString();
+                if (isInputsValid(firstName, lastName, phone, email)) {
+                    Contact contact;
+
+                    Contact currentlyViewing = mContactVM.getCurrentlyViewing().getValue();
+                    if (currentlyViewing != null) {
+                        // Edit
+                        contact = generateContactObject(
+                                currentlyViewing.getId(),
+                                firstName,
+                                lastName,
+                                phone,
+                                email,
+                                mContactVM.getImageInBase64(),
+                                currentlyViewing.isFavorite());
+                        mContactVM.updateContact(contact);
+                        mContactVM.setCurrentlyViewing(contact);
+                    } else {
+                        // New Contact
+                        contact = generateContactObject(
+                                0,
+                                firstName,
+                                lastName,
+                                phone,
+                                email,
+                                mContactVM.getImageInBase64(),
+                                false);
+                        mContactVM.addContact(contact);
+                    }
+
+                    navController.popBackStack();
+                } else {
+                    showAlert(ALERT_SAVE);
+                }
+            }
+        });
+
+        mContactVM.getCapturedImage().observe(getViewLifecycleOwner(), new Observer<Bitmap>() {
+            @Override
+            public void onChanged(Bitmap bitmap) {
+                binding.imgContact.setImageBitmap(Util.getRoundedCroppedBitmap(bitmap));
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> cameraActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                        Bitmap image = (Bitmap) data.getExtras().get("data");
+
+                        mContactVM.setCapturedImage(image);
+
+                    }
+                }
+            });
+
+    @Override
+    public void onDestroyView() {
+        mContactVM.getCapturedImage().removeObservers(getViewLifecycleOwner());
+
+        super.onDestroyView();
+    }
+
+    private boolean isInputsValid(String firstName, String lastName, String phone, String email) {
+
+        if (!firstName.isEmpty() || !lastName.isEmpty()) {
+            return !phone.isEmpty() || !email.isEmpty();
+        }
+        return false;
+
+    }
+
+    private Contact generateContactObject(int id, String firstName, String lastName, String phone, String email, String imgBase64, boolean isFavorite) {
+        return new Contact(
+                id,
+                firstName,
+                lastName,
+                phone,
+                email,
+                imgBase64,
+                isFavorite);
+    }
+
+    private void onBack() {
+        if (isFieldEmpty()) {
+            navController.popBackStack();
+        } else {
+            showAlert(ALERT_DISCARD);
+        }
+    }
+
+    private boolean isFieldEmpty() {
+        if (binding.edittextFirstName.getText().toString().trim().length() > 0)
+            return false;
+        if (binding.edittextLastName.getText().toString().trim().length() > 0)
+            return false;
+        if (binding.edittextPhone.getText().toString().trim().length() > 0)
+            return false;
+        if (binding.edittextEmail.getText().toString().trim().length() > 0)
+            return false;
+        return mContactVM.getImageInBase64() == null;
+    }
+
+    private void showAlert(int code) {
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(requireContext());
+        builder1.setCancelable(true);
+
+        switch (code) {
+            case ALERT_DISCARD:
+                builder1.setMessage(R.string.discard_alert);
+                builder1.setNeutralButton(
+                        getString(android.R.string.cancel),
+                        (dialog, id) -> {
+                            // do nothing
+                        });
+
+                builder1.setPositiveButton(
+                        getString(R.string.save),
+                        (dialog, id) -> {
+                            binding.btnSave.callOnClick();
+                        });
+
+                builder1.setNegativeButton(
+                        getString(R.string.discard),
+                        (dialog, id) -> {
+                            // go back
+                            navController.popBackStack();
+                        });
+                break;
+            case ALERT_SAVE:
+                builder1.setMessage(R.string.save_alert);
+                builder1.setPositiveButton(
+                        getString(android.R.string.ok),
+                        (dialog, id) -> {
+                            // do nothing
+                        });
+
+
+                break;
+        }
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
     }
 }
--- app/src/main/java/com/codev/recruitment/carlaberdin/vm/ContactViewModel.java
+++ app/src/main/java/com/codev/recruitment/carlaberdin/vm/ContactViewModel.java
@@ -1,6 +1,8 @@
 package com.codev.recruitment.carlaberdin.vm;
 
 import android.app.Application;
+import android.graphics.Bitmap;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.ObservableField;
@@ -11,23 +13,35 @@ import androidx.lifecycle.MutableLiveData;
 import com.codev.recruitment.carlaberdin.lib.ContactsLib;
 import com.codev.recruitment.carlaberdin.repository.data.Contact;
 import com.codev.recruitment.carlaberdin.utils.SingleLiveEvent;
+import com.codev.recruitment.carlaberdin.utils.Util;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ContactViewModel extends AndroidViewModel {
-
+    private static final String TAG = "ContactViewModel";
     private ContactsLib mContactsLib;
 
     private MutableLiveData<Contact> mCurrentlyViewing;
 
+    private SingleLiveEvent<Bitmap> imageCaptured;
+    private String imageInBase64;
+
+    private ExecutorService mExecutorService;
 
     public ContactViewModel(@NonNull Application application) {
         super(application);
+
+        mExecutorService = Executors.newSingleThreadExecutor();
 
         mContactsLib = new ContactsLib(application);
 
         mCurrentlyViewing = new MutableLiveData<>();
 
+        imageCaptured = new SingleLiveEvent<>();
     }
 
     public void addContact(Contact contact) {
@@ -51,10 +65,40 @@ public class ContactViewModel extends AndroidViewModel {
 
     public void resetCurrentlyViewing() {
         mCurrentlyViewing = new MutableLiveData<>();
+        imageCaptured = new SingleLiveEvent<>();
+        imageInBase64 = null;
     }
 
     public void setCurrentlyViewing(Contact currentlyViewing) {
         this.mCurrentlyViewing.postValue(currentlyViewing);
     }
 
+    public SingleLiveEvent<Bitmap> getCapturedImage() {
+        return imageCaptured;
+    }
+
+    public String getImageInBase64() {
+        return imageInBase64;
+    }
+
+    public void convertImageToBase64(Bitmap bitmap) throws IOException {
+        byte[] imgArr = Util.getBytes(bitmap, 50);
+        imageInBase64 = Util.encodeImageToBase64(imgArr);
+    }
+
+    public void setCapturedImage(Bitmap fullSize) {
+        mExecutorService.execute(() -> {
+            Bitmap rounded = Util.getRoundedCroppedBitmap(fullSize);
+            try {
+                convertImageToBase64(rounded);
+            } catch (IOException e) {
+                Log.d(TAG, "conversion to Base64 failed");
+            }
+            imageCaptured.postValue(rounded);
+        });
+    }
+
+    public void setCapturedImage(String base64) {
+        // imageCaptured.postValue();
+    }
 }
