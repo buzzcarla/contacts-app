ae8a321f764286470db021c6a6e4f2b62243f26c
Carla <berdincarla@gmail.com>
2023-09-25 12:36:00 +0800

    feat: ui: add/edit contact screen

--- app/src/main/java/com/codev/recruitment/carlaberdin/fragment/AddEditContactFragment.java
+++ app/src/main/java/com/codev/recruitment/carlaberdin/fragment/AddEditContactFragment.java
@@ -2,16 +2,23 @@ package com.codev.recruitment.carlaberdin.fragment;
 
 import android.os.Bundle;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codev.recruitment.carlaberdin.R;
+import com.codev.recruitment.carlaberdin.databinding.FragmentAddEditContactBinding;
+import com.codev.recruitment.carlaberdin.vm.ContactViewModel;
 
 public class AddEditContactFragment extends Fragment {
 
+    private FragmentAddEditContactBinding binding;
+
+    private ContactViewModel mContactVM;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,14 @@ public class AddEditContactFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_edit_contact, container, false);
+
+        binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_add_edit_contact, container, false);
+
+        View view = binding.getRoot();
+
+        mContactVM = new ViewModelProvider(requireActivity()).get(ContactViewModel.class);
+
+        return view;
     }
 }
